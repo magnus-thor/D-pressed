a3fa324a827300f259841a20f952d075f5c1dd19
AndreasMN <andycutter@godmail.dk>
2018-08-16 13:26:28 +0200

    made route for article new, made view, add controller action

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_16_081644) do
+ActiveRecord::Schema.define(version: 2018_08_16_095629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
--- features/step_definitions/paths_steps.rb
+++ features/step_definitions/paths_steps.rb
@@ -1,3 +1,7 @@
 When("I am on the landing page") do
     visit root_path
 end
+
+Given("I visit the Create article page") do
+    visit new_article_path
+end
