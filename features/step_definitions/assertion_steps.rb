ed06d5232ef019266a88e2f61a5cf64f57c4a474
Siianko <tsiyanko@gmail.com>
2018-08-16 18:13:20 +0200

    ad1 display on the landing page

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -5,3 +5,7 @@ end
 And("there should be a Article titled {string} in the database") do |expected_title|
     expect(Article.last.title).to eq expected_title
 end
+
+Then("I should see ad {string}") do |ad1|
+    expect(page).to have_css('.ad1')
+end
