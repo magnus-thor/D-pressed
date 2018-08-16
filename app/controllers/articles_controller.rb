73a9ba64860af92c807c2520be3f196705082553
AndreasMN <andycutter@godmail.dk>
2018-08-16 13:44:09 +0200

    add create method to article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,21 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.create(article_params)
+    if @article.persisted?
+      redirect_to root_path, notice: 'Article successfully created.'
+    else
+      flash[:error] = 'Your article could not be saved'
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
