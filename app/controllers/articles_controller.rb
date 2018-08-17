464df4b9f1f889530b8d0e3bd9f826a01571c1c0
Erik <erik.steinman@hotmail.com>
2018-08-17 14:02:56 +0200

    refactory the create method, add authenticate_user actions, add def show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,13 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!, only: [:show]
+  before_action :authenticate_user!, only: [:show, :create, :new]
 
   
   def index
     @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
   
   def new
@@ -11,15 +15,13 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.create(article_params)
+    @article = current_user.articles.create(article_params)
     if @article.persisted?
-      redirect_to root_path, notice: 'Article successfully created.'
+      flash[:notice] = 'Article successfully created.'
     else
-      flash[:error] = "Fields can't be blank. Your article could not be saved"
-      render :new
+      flash[:error] = 'Fields cant be blank. Your article could not be saved'
     end
   end
-
   private
 
   def article_params
