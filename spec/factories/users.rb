eedda5211bfe56e4115bc932dd01b1eab0250067
AndreasMN <andycutter@godmail.dk>
2018-08-17 09:59:46 +0200

    change email address for factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    sequence(:email) {|u| 'pablo@test.com'}    
+    sequence(:email) {|u| 'johnn@test.com'}    
     password 'my-password'
     password_confirmation 'my-password'
   end
