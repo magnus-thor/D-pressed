2cb826e89b09105aa9d218eb44ccc4b67da3ac18
Erik <erik.steinman@hotmail.com>
2018-08-17 14:05:44 +0200

    add show-route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root controller: :articles, action: :index
 
-  resources :articles, only: [:create, :new]
+  resources :articles, only: [:create, :new, :show]
 end
