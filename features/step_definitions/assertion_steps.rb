934bd20e79a25c47ed2112f52ef5afdc57f76196
Erik <erik.steinman@hotmail.com>
2018-08-19 09:38:03 +0200

    add step definition with Timecop

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -5,3 +5,8 @@ end
 And("there should be a Article titled {string} in the database") do |expected_title|
     expect(Article.last.title).to eq expected_title
 end
+
+Given("the date is {string}") do |date_string|
+    date = Date.parse(date_string)
+    Timecop.freeze(date)
+  end
