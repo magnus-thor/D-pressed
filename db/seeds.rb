59360fcc3581cbc44b5e28815eb5dfe9777b1c40
AndreasMN <andycutter@godmail.dk>
2018-08-20 22:31:51 +0200

    change title to use a faker that does not break the 200 character limit

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require 'faker'
 user = User.new(email: Faker::Internet.email, password: Faker::Number.number(10))
 
 10.times do
-    Article.create!(title: Faker::RickAndMorty.quote, 
+    Article.create!(title: Faker::Book.title, 
         body: Faker::Lorem.paragraphs(5),
         user: user)
 end
