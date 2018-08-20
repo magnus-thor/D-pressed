0aa94b098b4c158aa215df959badfef0ae1b04fe
AndreasMN <andycutter@godmail.dk>
2018-08-20 20:26:04 +0200

    add role with enum active record method, add 4 roles: user, subscriber, author and editor

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  enum role: [:user, :subscriber, :author, :editor]
   has_many :articles
+
+  after_initialize :set_default_role, :if => :new_record?
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def set_default_role
+    self.role ||= :user
+  end
 end
