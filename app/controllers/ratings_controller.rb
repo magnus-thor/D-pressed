7b72eb12a1620171a169a40e85fd451678c8627f
Erik <erik.steinman@hotmail.com>
2018-08-21 18:28:04 +0200

    add create action to controller

---
+++ app/controllers/ratings_controller.rb
@@ -0,0 +1,11 @@
+class RatingsController < ApplicationController
+
+    def create
+        @article = Article.find(params[:id])
+        @rating = article.ratings.create(value: params[:rating].to_i, user: current_user)
+        if rating.persisted?
+            flash[:notice] = 'Thanks for your vote'       
+        else
+            flash[:error] = 'An error occured. Your vote was not submitted' 
+        end
+    end
