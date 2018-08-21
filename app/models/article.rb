45e778da3a76a893a1a2dabf258d0a311ee2161b
Erik <erik.steinman@hotmail.com>
2018-08-21 19:10:00 +0200

    add has_many: ratings

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :ratings
   
   validates_presence_of :title, :body
   validates_length_of :title, maximum: 200
