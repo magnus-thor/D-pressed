62833a7b3838a73849754abf91e3aeed690958bc
AndreasMN <andycutter@godmail.dk>
2018-08-21 22:00:13 +0200

    add error notice for authorized error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
     include Pundit
     protect_from_forgery
+    rescue_from Pundit::NotAuthorizedError, with: :access_denied
+
+    private
+
+    def access_denied
+        redeirect_to root_path, notice: 'Access denied'
+    end
 end
