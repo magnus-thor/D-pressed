64e40f5030ae14d5f68bbd01552ef7adc6aa939e
AndreasMN <andycutter@godmail.dk>
2018-08-21 10:58:09 +0200

    remove require faker from seeds, use .create method on User instead of .new, made use of gem Database Cleaner we already had to purge DB before seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-user = User.new(email: Faker::Internet.email, password: Faker::Number.number(10))
+DatabaseCleaner.clean_with :truncation
+
+user = User.create(email: Faker::Internet.email, password: Faker::Number.number(10))
 
 10.times do
-    Article.create!(title: Faker::Book.title, 
+    Article.create(title: Faker::Book.title, 
         body: Faker::Lorem.paragraphs(5),
         user: user)
 end
