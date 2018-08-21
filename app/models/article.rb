64959072fcf59415da8e35bb59106784f8a67552
Erik <erik.steinman@hotmail.com>
2018-08-21 18:05:58 +0200

    add Associations and validatations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :comments
   
   validates_presence_of :title, :body
   validates_length_of :title, maximum: 200
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :articles
+  has_many :ratings
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
