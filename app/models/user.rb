f6cb0a360c9aea4e3f21f66b1124deee1a07d429
AndreasMN <andycutter@godmail.dk>
2018-08-21 13:04:59 +0200

    fix syntax error for roles, rspec goes green now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   after_initialize :set_default_role, :if => :new_record?
   
-  enum role: [{basic_user: 0}, {subscriber: 2}, {author: 4}, {editor: 8}]
+  enum role: {basic_user: 0, subscriber: 2, author: 4, editor: 8}
   
   has_many :articles
   
