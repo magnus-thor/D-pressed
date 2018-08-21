5cae594d93f2013423c2e0c2af42f659891dd5d6
AndreasMN <andycutter@godmail.dk>
2018-08-21 16:43:43 +0200

    add test for all roles

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,7 +20,9 @@ RSpec.describe User, type: :model do
 
   describe 'User roles' do
     let(:basic_user) {create :user, email: 'basic_user@test.se', role: :basic_user}
-    let(:subscriber) {create :user, email: 'author@test.se', role: :author}
+    let(:subscriber) {create :user, email: 'author@test.se', role: :subscriber}
+    let(:author) {create :user, email: 'author@test.se', role: :author}
+    let(:editor) {create :user, email: 'editor@test.se', role: :editor}
 
     it 'basic_user responds true if role is basic_user' do
       expect(basic_user.basic_user?).to eq true
@@ -28,6 +30,29 @@ RSpec.describe User, type: :model do
 
     it 'basic_user responds false if role is not basic_user' do
       expect(subscriber.basic_user?).to eq false
+    end
+    it 'subscriber responds true if role is subscriber' do
+      expect(subscriber.subscriber?).to eq true
+    end
+
+    it 'subscriber responds false if role is not basic_user' do
+      expect(subscriber.basic_user?).to eq false
+    end
+
+    it 'author responds true if role is author' do
+      expect(author.author?).to eq true
+    end
+
+    it 'author responds false if role is not author' do
+      expect(author.basic_user?).to eq false
+    end
+
+    it 'editor responds true if role is subscriber' do
+      expect(editor.editor?).to eq true
+    end
+
+    it 'editor responds false if role is not editor' do
+      expect(editor.basic_user?).to eq false
     end
   end
 end
