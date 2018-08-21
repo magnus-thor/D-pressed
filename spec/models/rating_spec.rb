a0dd9ad0b8f091f6cad03565106b9e5637a149ac
Erik <erik.steinman@hotmail.com>
2018-08-21 18:59:35 +0200

    add DB-table tests

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -3,5 +3,9 @@ require 'rails_helper'
 RSpec.describe Rating, type: :model do
   describe 'DB table' do
     it { is_expected.to have_db_column :value }
+    it { is_expected.to have_db_column :article_id }
+    it { is_expected.to have_db_column :user_id  }
+
+
   end
 end
