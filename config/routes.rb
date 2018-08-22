7c0a2950c7054be973e53dfdb67d7cc704bcb330
Siianko <tsiyanko@gmail.com>
2018-08-22 16:08:27 +0200

    localization fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root controller: :articles, action: :index
   resources :articles, only: [:create, :new, :show]
+  scope "(:locale)", locale: /en|se/ do
+    resources :books
+  end
 end
+
--- features/languages.feature
+++
@@ -1,6 +0,0 @@
-Feature: Internalization
-    As a user, 
-    In order to better understand the content on the web, 
-    I would like to have some language options. 
-
-    Scenario: Internalization
