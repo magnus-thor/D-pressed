1a11dacd7c63bbde5ffa6fc73371d4b3c5f6382f
AndreasMN <andycutter@godmail.dk>
2018-08-22 15:19:21 +0200

    add method to model, needs to be moved to user controller, when PR i merged

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,3 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
+
+def become_subscriber
+  if @user.basic_user?
+    @user.subscriber!, notice: 'You have succesfully subscribed to D-pressed'
+  else
+    @user.subscriber
+    notice: 'You are already subscribed'
+  end
+end
