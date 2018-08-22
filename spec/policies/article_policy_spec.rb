e9ee14b0a100e37fc5050ab931004b6ea77edebc
AndreasMN <andycutter@godmail.dk>
2018-08-22 12:26:46 +0200

    add test for :new

--- spec/policies/article_policy_spec.rb
+++ spec/policies/article_policy_spec.rb
@@ -8,26 +8,26 @@ RSpec.describe ArticlePolicy do
         let(:user) { create(:user, email: 'new@email.dk', role: 'basic_user') }
 
         it { is_expected.to permit_actions [:index] }
-        it { is_expected.to forbid_actions [:create, :show] }
+        it { is_expected.to forbid_actions [:create, :show, :new] }
     end
 
     context 'user is a subscriber' do
         let(:user) { create(:user, email: 'new@email.dk', role: 'subscriber') }
 
         it { is_expected.to permit_actions [:index, :show] }
-        it { is_expected.to forbid_actions [:create] }
+        it { is_expected.to forbid_actions [:create, :new] }
     end
 
     context 'user is a author' do
         let(:user) { create(:user, email: 'new@email.dk', role: 'author') }
 
-        it { is_expected.to permit_actions [:index, :show, :create] }
+        it { is_expected.to permit_actions [:index, :show, :create, :new] }
     end
 
     context 'user is a editor' do
         let(:user) { create(:user, email: 'new@email.dk', role: 'editor') }
 
-        it { is_expected.to permit_actions [:index, :show, :create] }
+        it { is_expected.to permit_actions [:index, :show, :create, :new] }
     end
 
 
