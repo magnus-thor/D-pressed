e574f2f8357ba31b44e39531ed44314a84da3efa
Erik <erik.steinman@hotmail.com>
2018-08-23 21:18:04 +0200

    add comment to user, edit view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_23_154714) do
+ActiveRecord::Schema.define(version: 2018_08_23_183927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,21 @@ ActiveRecord::Schema.define(version: 2018_08_23_154714) do
     t.bigint "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "replies", force: :cascade do |t|
+    t.string "content"
+    t.bigint "comment_id"
+    t.bigint "user_id"
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_replies_on_article_id"
+    t.index ["comment_id"], name: "index_replies_on_comment_id"
+    t.index ["user_id"], name: "index_replies_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -68,4 +82,8 @@ ActiveRecord::Schema.define(version: 2018_08_23_154714) do
 
   add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
+  add_foreign_key "comments", "users"
+  add_foreign_key "replies", "articles"
+  add_foreign_key "replies", "comments"
+  add_foreign_key "replies", "users"
 end
