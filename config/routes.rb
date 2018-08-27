ddd169b4a920feeebc6937cd939a4a5d44458fe7
Erik <erik.steinman@hotmail.com>
2018-08-27 19:44:50 +0200

    config namespaces :api in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,9 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|se/ do
     resources :books
   end
+
+  namespace :api do
+    mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+  end
 end
 
