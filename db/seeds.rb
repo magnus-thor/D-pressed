6d9c37aad56d846ff18f1b13082029ca2ca8b3af
AndreasMN <andycutter@godmail.dk>
2018-08-27 10:19:27 +0200

    add user editor@dpressed.se to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 User.all.each {|user| user.destroy}
 Article.all.each {|article| article.destroy}
 
+editor = User.create(email: 'editor@dpressed.se', password: '1234qwer', role: 'editor')
 user = User.create(email: Faker::Internet.email, password: Faker::Number.number(10))
 
 10.times do
