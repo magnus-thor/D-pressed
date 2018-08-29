807cc5e5e413ecfbb5b50e027f20ddbc40560cbf
AndreasMN <andycutter@godmail.dk>
2018-08-29 15:56:11 +0200

    modify ecpected output to match..again

--- spec/requests/api/sessions_spec.rb
+++ spec/requests/api/sessions_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe 'Sessions', type: :request do
             expected_response = {
                 'data' => {
                     'id' => user.id, 'uid' => user.email, 'email' => user.email,
-                    'provider' => 'email', 'role' => user.role, "latitude"=>nil, "longitude"=>nil
+                    'provider' => 'email', 'role' => user.role
                 }
             }
 
